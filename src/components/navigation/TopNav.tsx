f791d1dba93ea6b277b3552adebddfd645ef2407
Vxrcel <skyeparagon@gmail.com>
2024-04-20 15:50:40 +0100

    auth: with clerk

--- src/components/navigation/TopNav.tsx
+++ src/components/navigation/TopNav.tsx
@@ -1,9 +1,19 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const TopNav = () => {
   return (
-    <nav className="flex justify-between border-b border-gray-900 px-4 py-4 md:px-8">
-      <h2 className="font-inter text-xl font-bold">Gallery</h2>
+    <nav className="font-inter flex justify-between border-b border-gray-900 px-4 py-4 text-xl font-bold md:px-8">
+      <h2 className="">Gallery</h2>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </nav>
   );
 };
--- src/components/welcome/LoadingScreen.tsx
+++ src/components/welcome/LoadingScreen.tsx
@@ -1,11 +1,11 @@
 const LoadingScreen = () => {
   return (
     <section className="bg-primary fixed inset-0 grid h-screen w-full place-items-center">
-      <div className="flex items-center gap-x-2">
-        <p className="font-inter w-fit bg-gradient-to-r from-white via-blue-200  to-blue-500 bg-clip-text font-medium text-transparent">
+      <div className="flex items-center gap-x-2 text-3xl lg:text-4xl">
+        <p className="font-inter w-fit bg-gradient-to-r from-white via-blue-200  to-blue-500 bg-clip-text font-medium text-transparent ">
           VXRCEL
         </p>
-        <span className="lg:text-2xl">GALLERY</span>
+        <span>GALLERY</span>
       </div>
     </section>
   );
