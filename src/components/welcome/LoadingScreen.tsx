e6d52272c3a617a6fe131ab6f518ffe72183f173
Vxrcel <skyeparagon@gmail.com>
2024-04-23 12:44:52 +0100

    added shadUI

--- src/components/welcome/LoadingScreen.tsx
+++ src/components/welcome/LoadingScreen.tsx
@@ -1,8 +1,8 @@
 const LoadingScreen = () => {
   return (
-    <section className="bg-primary fixed inset-0 grid h-screen w-full place-items-center">
+    <section className="fixed inset-0 grid h-screen w-full place-items-center bg-gray-950">
       <div className="flex items-center gap-x-2 text-3xl lg:text-4xl">
-        <p className="font-inter w-fit bg-gradient-to-r from-white via-blue-200  to-blue-500 bg-clip-text font-medium text-transparent ">
+        <p className="w-fit bg-gradient-to-r from-white via-blue-200 to-blue-500  bg-clip-text font-inter font-medium text-transparent ">
           VXRCEL
         </p>
         <span>GALLERY</span>
